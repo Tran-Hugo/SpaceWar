543cf7f43089d7eefed40b5ba44519ea97e3fb42
Anisse <anissbouainbi@hotmail.it>
2023-11-11 17:17:35 +0100

    basic movement system + shoothing system

---
+++ Config.py
@@ -0,0 +1,43 @@
+# singleton class for configuration
+import pygame
+
+class Config():
+    __instance = None
+    __width = 720
+    __height = 480
+    __fps = 60
+    __screen = None
+    
+    @staticmethod
+    def getInstance():
+        if Config.__instance == None:
+            Config()
+        return Config.__instance
+
+    def __init__(self):
+        print("init")
+        if Config.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Config.__instance = self
+            Config.__width = 720
+            Config.__height = 480
+            Config.__fps = 60
+            Config.__screen = pygame.display.set_mode((Config.__width, Config.__height))
+    def getWidth(self):
+        return self.__width
+    def setWidth(self, width):
+        self.__width = width
+
+    def getHeight(self):
+        return self.__height
+    def getFps(self):
+        return self.__fps
+    
+
+    def getScreen(self):
+        return self.__screen
+    def setScreen(self, screen):
+        self.__screen = screen
+    def regenerateScreen(self):
+        self.__screen = pygame.display.set_mode((self.__width, self.__height))
--- entities/Ship.py
+++ entities/Ship.py
@@ -1,12 +1,15 @@
 import pygame
 import os
+from Config import Config
 
 class Ship():
-    def __init__(self, x, y):
+    def __init__(self):
+        self.config = Config.getInstance()
         self.player = pygame.image.load(os.path.join("assets", "player.png"))
-        self.rect = self.player.get_rect(x=x, y=y)
-        self.speed =5
+        self.rect = self.player.get_rect(x=self.config.getWidth() / 2, y=self.config.getHeight() / 2)
+        self.speed = 4
         self.velocity = [0,0]
+        self.bullets = []
     
     def move(self):
         self.rect.move_ip(self.velocity[0] * self.speed, self.velocity[1] * self.speed)
