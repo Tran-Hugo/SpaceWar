5e8f3ce6d082dc3e13e2310af63787842518b9db
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2023-11-13 10:15:32 +0100

    added small rocks, added ship.shoot

--- scenes/MainScene.py
+++ scenes/MainScene.py
@@ -16,28 +16,23 @@ class MainScene(BaseScene):
         self.explosion_group = pygame.sprite.Group()
     
     def ProcessInput(self, events, pressed_keys):
-        if pressed_keys[pygame.K_UP] and self.ship.rect.y > 0 :
+        if pressed_keys[pygame.K_z] and self.ship.rect.y > 0 :
             self.ship.velocity[1] = -1
-        elif pressed_keys[pygame.K_DOWN] and self.ship.rect.y < self.config.getHeight() - self.ship.rect.height :
+        elif pressed_keys[pygame.K_s] and self.ship.rect.y < self.config.getHeight() - self.ship.rect.height :
             self.ship.velocity[1] = 1
         else:
             self.ship.velocity[1] = 0
 
-        if pressed_keys[pygame.K_LEFT] and self.ship.rect.x > 0 :
+        if pressed_keys[pygame.K_q] and self.ship.rect.x > 0 :
             self.ship.velocity[0] = -1
-        elif pressed_keys[pygame.K_RIGHT] and self.ship.rect.x < self.config.getWidth() - self.ship.rect.width:
+        elif pressed_keys[pygame.K_d] and self.ship.rect.x < self.config.getWidth() - self.ship.rect.width:
             self.ship.velocity[0] = 1
         else:
             self.ship.velocity[0] = 0
 
         for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    opposite = mouse_y - self.ship.rect.y
-                    adjacent = mouse_x - self.ship.rect.x
-                    angle = math.atan2(opposite, adjacent)
-                    self.ship.bullets.append(Bullet(self.ship.rect.x, self.ship.rect.y, angle))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.ship.shoot()
 
     def Update(self):
         self.ship.move()
